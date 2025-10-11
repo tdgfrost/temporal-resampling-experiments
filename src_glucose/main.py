119ee5f2acc09eeeb48e8d89fb53fbea336d34fb
tdgfrost <100413067+tdgfrost@users.noreply.github.com>
2025-10-11 12:09:04 +0100

    Corrected epochs

--- src_glucose/main.py
+++ src_glucose/main.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
 
             log_dict = algo.fit(
                 dataset=replay_buffer_env,
-                n_epochs_train=500,
+                n_epochs_train=500 if DECOY_INTERVAL in [0, 2] else 5,
                 n_epochs_eval=1,
                 evaluators=evaluators,
                 decoy_interval=DECOY_INTERVAL,
